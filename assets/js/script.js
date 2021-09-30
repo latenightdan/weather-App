2383db174016788e49a0be98f61001606bf455d6
daniel natale <dnatale667@gmail.com>
2021-09-30 17:59:22 -0400

    debuggin api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,41 @@
-var userInputEl = document.querySelector("#user-form");
-var weatherInput = document.getElementById("#city");
-var city = document.querySelector("#city");
-var weatherBlockEl = document.querySelector("#weather-block-title");
-var humidityEl = document.querySelector("#humid");
-var tempEl = document.querySelector("#temperature");
-var windEl = document.querySelector("#windd");
-function userInputHandler(event) {
-    event.preventDefault();
-    var cityData = city.value.trim();
-    if (cityData) {
-        city.value = "";
-        getWeatherData(cityData);
+// var userInputEl = document.querySelector("#user-form");
+// var weatherInput = document.getElementById("#city");
+// var city = document.querySelector("#city");
+// var weatherBlockEl = document.querySelector("#weather-block-title");
+// var humidityEl = document.querySelector("#humid");
+// var tempEl = document.querySelector("#temperature");
+// var windEl = document.querySelector("#windd");
+
+// function userInputHandler(event) {
+//     event.preventDefault();
+//     var cityData = city.value.trim();
+//     if (cityData) {
+//         city.value = "";
+//         getWeatherData(cityData);
 
 
-        // console.log(cityData)
-        // console.log(city.value);
+//         // console.log(cityData)
+//         // console.log(city.value);
 
-    }
-    else {
+//     }
+//     else {
 
-    }
+//     }
 
-}
+// }
 
-userInputEl.addEventListener("submit", userInputHandler);
+// userInputEl.addEventListener("submit", userInputHandler);
 
 
 
 
 var getWeatherData = function (city) {
-    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=0c37646b1c308ae2462fc8547af5cdd4"
+    var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=0c37646b1c308ae2462fc8547af5cdd4"
     fetch(weatherApi).then(function (response) {
         response.json().then(function (data) {
-            console.log(data);
-            console.log(city);
+            // console.log(data);
+            // console.log(city);
+            // console.log(data);
             displayWeather(data, city);
         });
     });
@@ -41,30 +43,31 @@ var getWeatherData = function (city) {
 };
 
 var displayWeather = function (weather, city) {
-    console.log(weather.wind.speed);
+    console.log(weather.list[0].main.humidity);
     console.log(city);
-  weatherBlockEl.textContent = city;
-  humidityEl.textContent = "";
-  var humidityy = weather.main.humidity;
-  var humidEl = document.createElement("span");
-  humidEl.textContent = humidityy + "%";
-humidityEl.appendChild(humidEl);
+    console.log(weather);
+//   weatherBlockEl.textContent = city;
+//   humidityEl.textContent = "";
+//   var humidityy = weather.main.humidity;
+//   var humidEl = document.createElement("span");
+//   humidEl.textContent = humidityy + "%";
+// humidityEl.appendChild(humidEl);
 
-tempEl.textContent = "";
-  var temperaturee = weather.main.humidity;
-  var tempyyEl = document.createElement("span");
-  tempyyEl.textContent = temperaturee + "%";
-tempEl.appendChild(tempyyEl);
+// tempEl.textContent = "";
+//   var temperaturee = weather.main.humidity;
+//   var tempyyEl = document.createElement("span");
+//   tempyyEl.textContent = temperaturee + "%";
+// tempEl.appendChild(tempyyEl);
 
-windEl.textContent = "";
-var windd = weather.wind.speed;
-var windyEl = document.createElement("span");
-windyEl.textContent = windd + "mph";
-windEl.appendChild(windyEl);
+// windEl.textContent = "";
+// var windd = weather.wind.speed;
+// var windyEl = document.createElement("span");
+// windyEl.textContent = windd + "mph";
+// windEl.appendChild(windyEl);
 
 
 }
 
-getWeatherData("london");
+getWeatherData("red bank");
 
 

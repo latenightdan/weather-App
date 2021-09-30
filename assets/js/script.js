8cc21d1dbd17159ad39208e5ae4d4acd3938c766
daniel natale <dnatale667@gmail.com>
2021-09-30 17:31:37 -0400

    api working with values, but wrong api for weekly forcastgit add -Agit add -A

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,70 @@
 var userInputEl = document.querySelector("#user-form");
-var weatherInput = document.getElementById("#weather");
-
-function userInputHandler(event){
+var weatherInput = document.getElementById("#city");
+var city = document.querySelector("#city");
+var weatherBlockEl = document.querySelector("#weather-block-title");
+var humidityEl = document.querySelector("#humid");
+var tempEl = document.querySelector("#temperature");
+var windEl = document.querySelector("#windd");
+function userInputHandler(event) {
     event.preventDefault();
-   
-    console.log(event.text);
+    var cityData = city.value.trim();
+    if (cityData) {
+        city.value = "";
+        getWeatherData(cityData);
+
+
+        // console.log(cityData)
+        // console.log(city.value);
+
+    }
+    else {
+
+    }
+
 }
 
 userInputEl.addEventListener("submit", userInputHandler);
 
-var getWeatherData = function(){
-    var weatherApi = fetch("https://api.openweathermap.org/data/2.5/weather?q=Orlando,us&APPID=0c37646b1c308ae2462fc8547af5cdd4").then(function(response) {
-        response.json().then(function(data){
-            console.log(data.wind);
-            
+
+
+
+var getWeatherData = function (city) {
+    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=0c37646b1c308ae2462fc8547af5cdd4"
+    fetch(weatherApi).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+            console.log(city);
+            displayWeather(data, city);
         });
-      });
-   
+    });
+
 };
 
-getWeatherData();
+var displayWeather = function (weather, city) {
+    console.log(weather.wind.speed);
+    console.log(city);
+  weatherBlockEl.textContent = city;
+  humidityEl.textContent = "";
+  var humidityy = weather.main.humidity;
+  var humidEl = document.createElement("span");
+  humidEl.textContent = humidityy + "%";
+humidityEl.appendChild(humidEl);
 
-var displayWeatherBox = function(){
+tempEl.textContent = "";
+  var temperaturee = weather.main.humidity;
+  var tempyyEl = document.createElement("span");
+  tempyyEl.textContent = temperaturee + "%";
+tempEl.appendChild(tempyyEl);
+
+windEl.textContent = "";
+var windd = weather.wind.speed;
+var windyEl = document.createElement("span");
+windyEl.textContent = windd + "mph";
+windEl.appendChild(windyEl);
 
 
 }
+
+getWeatherData("london");
+
+
